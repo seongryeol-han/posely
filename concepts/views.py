4c1ba71f8504dad9f642b552bf2d62ab9f8170f3
eomjunseong <66054183+eomjunseong@users.noreply.github.com>
2021-05-03 12:45:00 +0900

    05-03 studio_list_modified

--- concepts/views.py
+++ concepts/views.py
@@ -1,11 +1,16 @@
 from django.views.generic import DetailView, ListView
 from django.shortcuts import render
+from django.urls import reverse
 from . import models
 
 # Create your views here.
 
 
-class SelectConcept(DetailView):
+def SelectConcept(request,pk):
     """SelectConcept Definition"""
 
-    model = models.Concept
+    
+    concept = models.Concept.objects.get(pk=pk)
+    aa = concept.studio.name
+    print(concept,aa)
+    return render(request, "studios/detail.html", {})
--- studios/urls.py
+++ studios/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from concepts import views as concept_views
+from studios import views as studios_views
 
 app_name = "studios"
 
 urlpatterns = [
-    path("<int:pk>", concept_views.SelectConcept.as_view(), name="concept"),
+    path("<int:potato>", studios_views.SelectStudio.as_view(), name="studio"),
     path("search/", views.SearchView.as_view(), name="search"),
 ]
