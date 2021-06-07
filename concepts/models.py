bd1fdc84f826da002f8f333f14fad133f3a23914
seongryeol-han <root@DESKTOP-JRHM3NP.localdomain>
2021-06-07 23:00:12 +0900

    correct models fields

--- concepts/models.py
+++ concepts/models.py
@@ -5,7 +5,7 @@ from core import models as core_models
 
 
 class Photo(core_models.TimeStampedModel):
-    """ Photo Model Definition"""
+    """Photo Model Definition"""
 
     caption = models.CharField(max_length=80, default="")
     file = models.ImageField(upload_to="concept_photos")  # /uploads/concept_photos에 저장.
@@ -16,7 +16,6 @@ class Photo(core_models.TimeStampedModel):
         "studios.Studio",
         related_name="photos",
         on_delete=models.CASCADE,
-        default=False,
     )
 
     def __str__(self):
@@ -27,14 +26,13 @@ class Concept(core_models.TimeStampedModel):
     """Concept Model Definition"""
 
     name = models.CharField(max_length=30)
-    concept_description = models.TextField()
+    concept_description = models.TextField(default="", blank=True)
+    service_config = models.TextField(default="", blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=0)
-    service_config = models.TextField()
     studio = models.ForeignKey(
         "studios.Studio",
         related_name="concepts",  # studio에서 concept을 "concepts"으로 사용하면 된다.
         on_delete=models.CASCADE,
-        default=False,
     )
 
     def __str__(self):
