989a5c8b49555f9c3e0921adcf86d07b811d4b4c
seongryeol-han <root@DESKTOP-JRHM3NP.localdomain>
2021-07-07 23:39:16 +0900

    210707_1

--- concepts/urls.py
+++ concepts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "concepts"
 
 urlpatterns = [
-    path("<int:pk>", views.ConceptDetail.as_view(), name="concept"),
+    path("<int:pk>", views.ConceptDetail.as_view(), name="detail"),
     path("edit-list", views.ConceptEditListView.as_view(), name="edit-list"),
     path("<int:pk>/edit/", views.EditConceptView.as_view(), name="edit"),
     path("<int:pk>/photos/", views.ConceptPhotosView.as_view(), name="photos"),
