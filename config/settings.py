b7702680a185d7eddd6314c86b6e8f610737256e
seongryeol-han <root@DESKTOP-JRHM3NP.localdomain>
2021-07-18 11:17:09 +0900

    210718

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "%9ec@=u6wcl=e4t9n+gu0(suc2$yrx@82z8dp96rsb5uf4!*d#"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
